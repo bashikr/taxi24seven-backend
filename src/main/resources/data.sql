0d6c26fc0df2b91f3cdb4421609e5568ab312257
Bashar <bashikr@gmail.com>
2022-07-18 22:35:40 +0200

    untrack files contained in the .gitignore file

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into users (id, first_name, last_name, username, phone, email, password, role, join_date)
-values(1, 'admin', 'admin', 'bash', '234', 'test@tg.com', '123', 'admin', '2020-01-02')
